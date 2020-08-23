d99fc4ecbcca66ab507af344b00c1a4fd8fdf808
Marcos Cardoso <dev.marcoscardoso@gmail.com>
2020-08-23 12:26:03 -0300

    Navegação

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
 
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+
 import styles from './styles';
 import landingImage from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
@@ -8,6 +11,12 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 
 function Landing() {
+    const navigation = useNavigation();
+
+    function handleNavigateToGiveClassesPage() {
+        navigation.navigate('GiveClasses');
+    }
+
     return(
         <View style={styles.container}>
             <Image source={landingImage} style={styles.banner}/>
@@ -18,14 +27,17 @@ function Landing() {
                 </Text>
             </Text>
             <View style={styles.buttonsContainer}>
-                <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+                <RectButton 
+                    style={[styles.button, styles.buttonPrimary]}>
                     <Image source={studyIcon} />
                     <Text style={styles.buttonText}>Estudar</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+                </RectButton>
+                <RectButton 
+                    onPress={handleNavigateToGiveClassesPage} 
+                    style={[styles.button, styles.buttonSecondary]}>
                     <Image source={giveClassesIcon} />
                     <Text style={styles.buttonText}>Dar aulas</Text>
-                </TouchableOpacity>
+                </RectButton>
             </View>
 
             <Text style={styles.totalConnections}>
