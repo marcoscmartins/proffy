2ad9f9c80b4ab2e2f82f2e778989b8a1c7774414
Marcos Cardoso <dev.marcoscardoso@gmail.com>
2020-08-23 12:49:31 -0300

    Página Dar Aulas

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,8 +1,40 @@
 import React from 'react';
+import { View, ImageBackground, Text } from 'react-native';
+import styles from './styles';
 
-import { View } from 'react-native';
+import giveClassesBgImg from '../../assets/images/give-classes-background.png';
+import backIcon from '../../assets/images/icons/back.png';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 function GiveClasses() {
-    return <View />
+    // const navigation = useNavigation();
+    const { goBack } = useNavigation();
+
+    function handleNavigateBack() {
+        goBack();
+    }
+    return (
+        <View style={styles.container}>
+            <ImageBackground 
+                resizeMode='contain' 
+                source={giveClassesBgImg} 
+                style={styles.content}>
+                <Text style={styles.title}>
+                    Quer ser um Proffy?
+                </Text>
+                <Text style={styles.description}>
+                    Para começar, você precisa se cadastrar como professor na nossa plataforma web.
+                </Text>
+            </ImageBackground>
+            <RectButton 
+                onPress={handleNavigateBack}
+                style={styles.okButton}>
+                <Text style={styles.okButtonText}>
+                    Tudo bem
+                </Text>
+            </RectButton>
+        </View>
+    )
 }
 export default GiveClasses;
--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -5,10 +5,11 @@ import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
 
 import styles from './styles';
-import landingImage from '../../assets/images/landing.png';
-import studyIcon from '../../assets/images/icons/study.png';
-import giveClassesIcon from '../../assets/images/icons/give-classes.png';
-import heartIcon from '../../assets/images/icons/heart.png';
+
+import landingImage     from '../../assets/images/landing.png';
+import studyIcon        from '../../assets/images/icons/study.png';
+import giveClassesIcon  from '../../assets/images/icons/give-classes.png';
+import heartIcon        from '../../assets/images/icons/heart.png';
 
 function Landing() {
     const navigation = useNavigation();
