1ac61ab33439f28750343f9234e9176ce1d5d2c9
Marcos Cardoso <dev.marcoscardoso@gmail.com>
2020-08-29 10:46:56 -0300

    Conexão com API e Whatsapp deep linking

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, Linking } from 'react-native';
 
 import styles from './styles';
 import { RectButton } from 'react-native-gesture-handler';
@@ -8,36 +8,52 @@ import heartOutlineIcon from '../../assets/images/icons/heart-outline.png'
 import unfavoriteIcon from '../../assets/images/icons/unfavorite.png'
 import whatsappIcon from '../../assets/images/icons/whatsapp.png'
 
-function TeacherItem() {
+export interface Teacher  {
+    id: number;
+    name: string;
+    avatar: string;
+    bio: string;
+    cost: number;
+    whatsapp: string;
+    subject: string;
+}
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem:React.FC<TeacherItemProps> = ({ teacher }) => {
+
+    function handleLinkToWhatsapp() {
+        Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}"`);
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.profile}>
                 <Image
                     style={styles.avatar}
-                    source={{ uri: 'https://github.com/marcoscmartins.png' }}
+                    source={{ uri: teacher.avatar }}
                 />
-                <View style={styles.profileInfo}>
-                    <Text style={styles.name}>Marcos Cardoso</Text>
-                    <Text style={styles.subject}>Programação</Text>
+                <View style={styles.profileInfo}> 
+                    <Text style={styles.name}>{ teacher.name }</Text>
+                    <Text style={styles.subject}>{ teacher.subject }</Text>
                 </View>
             </View>
             <Text style={styles.bio}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                {'\n'}{'\n'} 
-                Dicta, repudiandae officia! Odit, nobis perspiciatis laborum quasi voluptas aliquid fugit doloribus! 
+                {teacher.bio}
             </Text>
 
             <View style={styles.footer}>
                 <Text style={styles.price}>
                     Preço/hora { '   ' }
-                    <Text style={styles.priceValue}>R$ 20,00</Text>
+                    <Text style={styles.priceValue}>{teacher.cost}</Text>
                 </Text>
                 <View style={styles.buttonsContainer}>
                     <RectButton style={[styles.favoriteButton, styles.favorited]}>
                         {/* <Image source={heartOutlineIcon} /> */}
                         <Image source={unfavoriteIcon} />
                     </RectButton>
-                    <RectButton style={styles.contactButton}>
+                    <RectButton onPress={handleLinkToWhatsapp} style={styles.contactButton}>
                         <Image source={whatsappIcon} />
                         <Text style={styles.contactButtonText}>Entrar em contato</Text>
                     </RectButton>
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -3,17 +3,37 @@ import { View, Text, TextInput, ScrollView } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 import styles from './styles';
+import api from '../../services/api';
 
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
 
 function TeacherList() {
 
     const [ isFiltersVisible, setIsFiltersVisible ] = useState(false);
 
+    const [subject, setSubject] = useState('');
+    const [week_day, setWeekday] = useState('');
+    const [time, setTime] = useState('');
+
+    const [teachers, setTeachers] = useState([]);
+
     function handleToggleFiltersVisible() {
         setIsFiltersVisible(!isFiltersVisible);
+    }
+
+    async function handleFiltersSubmit() {
+        const response = await api.get('classes', {
+            params: {
+                subject,
+                week_day,
+                time
+            }
+        });
+
+        setIsFiltersVisible(false); 
+        setTeachers(response.data);
     }
 
     return (
@@ -30,6 +50,8 @@ function TeacherList() {
                         <Text style={styles.label}>Matéria</Text>
                         <TextInput 
                             style={styles.input}
+                            value={subject}
+                            onChangeText={text => setSubject(text)}
                             placeholder="Qual a matéria?"
                             placeholderTextColor= '#c1bccc'
                         />
@@ -38,6 +60,8 @@ function TeacherList() {
                                 <Text style={styles.label}>Dia da semana</Text>
                                 <TextInput 
                                     style={styles.input}
+                                    value={week_day}
+                                    onChangeText={text => setWeekday(text)}
                                     placeholder="Qual o dia?"
                                     placeholderTextColor= '#c1bccc'
                                 />
@@ -46,12 +70,16 @@ function TeacherList() {
                                 <Text style={styles.label}>Horário</Text>
                                 <TextInput 
                                     style={styles.input}
+                                    value={time}
+                                    onChangeText={text => setTime(text)}
                                     placeholder="Qual o horário?"
                                     placeholderTextColor= '#c1bccc'
                                 />
                             </View>
                         </View>
-                        <RectButton style={styles.submitButton}>
+                        <RectButton 
+                            onPress={handleFiltersSubmit}
+                            style={styles.submitButton}>
                             <Text style={styles.submitButtonText}>Filtrar</Text>
                         </RectButton>
                     </View>
@@ -65,13 +93,9 @@ function TeacherList() {
                     paddingBottom: 16,
                 }}
             >
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
+                {teachers.map((teacher:Teacher) => {
+                    return <TeacherItem key={teacher.id} teacher={teacher} />
+                })}
             </ScrollView>
         </View>
     );
